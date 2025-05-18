f91864eeba78dd0f830e7732a5ce28d3af4c4176
Salvatore Calo <dafder45678@gmail.com>
2025-05-18 19:48:09 +0200

    feat: add background map

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import { Pippo } from './components/'
+
+function App() {
+  return (
+    <section style={{height: "100vh"}}>
+      <Pippo />
+    </section>
+  )
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export {Pippo} from './Map/Map'
